d4f137a5bda5c14f34bafa9bc9969155145f7e69
Joao4Pedroc <joao4pedroc@gmail.com>
2024-09-23 15:03:48 -0300

    Feat: responsive

--- app/_components/ExperiencesCard.tsx
+++ app/_components/ExperiencesCard.tsx
@@ -7,33 +7,33 @@ function ExperiencesCard() {
         <li className="mb-12">
           <a
             href="#"
-            className="grid grid-cols-[20%_80%] gap-8 grid-row-4 mt-24 hover:bg-zinc-600 rounded-md p-4 hover:shadow-md transition-all"
+            className="grid grid-cols-[20%_80%] place-content-center gap-8 grid-row-4 mt-24  hover:bg-zinc-600 rounded-lg p-4 pl-12 lg:pl-16 hover:shadow-md transition-all"
           >
             <Image
               src={"/logo-FEPECS.png"}
               width={150}
               height={50}
-              className="rounded-lg row-span-2 place-self-center w-full bg-white "
+              className="rounded-lg row-span-2 row-start-4 col-span-2  lg:row-span-2 lg:col-start-1 lg:row-start-1 lg:col-span-1 bg-white "
               alt="FEPECS Agendamento de salas"
               loading="lazy"
               layout="responsive"
-              objectFit="cover"
+              objectFit="contain"
               decoding="async"
             />
-            <span className="font-bold">
+            <span className="font-bold col-span-2 lg:col-start-2">
               Intern, FEPECS
               <span className="opacity-50 text-zinc-400">
                 {" "}
                 (2024 - PRESENT)
               </span>
             </span>
-            <p className="row-start-2 col-start-2 opacity-75 text-zinc-400">
+            <p className="row-start-2 col-start-1 col-span-2 opacity-75 text-zinc-400 lg:col-start-2">
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus
               sunt, reprehenderit illum suscipit iure corporis, unde aliquid
               nemo odio voluptatibus, eos tempore. Debitis fugiat magnam eum
               eius autem obcaecati. Nihil.
             </p>
-            <ul className="flex flex-wrap gap-4 col-start-2 row-start-3">
+            <ul className="flex flex-wrap gap-4 col-start-1 col-span-3 row-start-3 lg:col-start-2">
               <li className="px-3 py-2 leading-5 text-xs font-medium rounded-full text-amber-300 bg-amber-400/10 ">
                 WordPress
               </li>
--- app/_components/ProjectsCard.tsx
+++ app/_components/ProjectsCard.tsx
@@ -7,27 +7,29 @@ function ProjectsCard() {
         <li className="mb-12">
           <a
             href="#"
-            className="grid grid-cols-[20%_80%] gap-8 grid-row-4 mt-24 hover:bg-zinc-600 rounded-md p-4 hover:shadow-md transition-all"
+            className="grid grid-cols-[20%_80%] place-content-center gap-8 grid-row-4 mt-24  hover:bg-zinc-600 rounded-lg p-4 pl-12 lg:pl-16 hover:shadow-md transition-all "
           >
             <Image
               src={"/Agendamento-de-salas.jpeg"}
               width={200}
               height={50}
-              className="rounded-lg row-span-2 place-self-center w-full "
+              className="rounded-lg row-span-2 row-start-4 col-span-2  lg:row-span-2 lg:col-start-1 lg:row-start-1 lg:col-span-1 "
               alt="FEPECS Agendamento de salas"
               loading="lazy"
               layout="responsive"
-              objectFit="cover"
+              objectFit="contain"
               decoding="async"
             />
-            <span className="font-bold">Agendamento de Salas FEPECS</span>
-            <p className="row-start-2 col-start-2 opacity-75 text-zinc-400">
+            <span className="font-bold col-span-2 lg:col-start-2">
+              Agendamento de Salas FEPECS
+            </span>
+            <p className="row-start-2 col-start-1 col-span-2 opacity-75 text-zinc-400 lg:col-start-2">
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus
               sunt, reprehenderit illum suscipit iure corporis, unde aliquid
               nemo odio voluptatibus, eos tempore. Debitis fugiat magnam eum
               eius autem obcaecati. Nihil.
             </p>
-            <ul className="flex flex-wrap gap-4 col-start-2 row-start-3">
+            <ul className="flex flex-wrap gap-4 col-start-1 col-span-3 row-start-3 lg:col-start-2">
               <li className="px-3 py-2 leading-5 text-xs font-medium rounded-full text-amber-300 bg-amber-400/10 ">
                 Typescript
               </li>
