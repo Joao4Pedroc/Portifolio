73eae9bc0b1369edeeeb0eae067ae646f3b3900e
Joao4Pedroc <joao4pedroc@gmail.com>
2024-09-24 15:58:13 -0300

    Fix: spacing

--- app/page.tsx
+++ app/page.tsx
@@ -3,64 +3,60 @@ import ExperiencesCard from "./_components/ExperiencesCard";
 
 export default function Home() {
   return (
-    <div className="lg:pb-28 pb-16">
-      <span>
-        Hello, my name is João Pedro, and I&apos;m a passionate web developer
-        who loves learning new technologies and constantly improving my skills.
-        <br />
-        <br />
-        Here, you can explore my expertise, favorite tools, and a selection of
-        my best projects. To see all of my work, feel free to visit my GitHub.
-      </span>
+    <div className="lg:pb-28 pb-16 flex flex-col">
       <div>
-        <div className="flex flex-col gap-24 transition-all ">
-          <div className="text-center font-bold text-4xl lg:text-xl mt-20">
-            My Experiences:
-          </div>
-          <ExperiencesCard
-            href="#"
-            imageData={{ src: "/logo-FEPECS.png", alt: "logo FEPECS" }}
-            name="Intern FEPECS"
-            duration={{ start: "2024", end: "CURRENT" }}
-            description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus
+        <span>
+          Hello, my name is João Pedro, and I&apos;m a passionate web developer
+          who loves learning new technologies and constantly improving my
+          skills.
+          <br />
+          <br />
+          Here, you can explore my expertise, favorite tools, and a selection of
+          my best projects. To see all of my work, feel free to visit my GitHub.
+        </span>
+      </div>
+
+      <div className="flex-col flex gap-12">
+        <div className="text-center font-bold text-4xl lg:text-xl mt-20">
+          My Experiences:
+        </div>
+        <ExperiencesCard
+          href="#"
+          imageData={{ src: "/logo-FEPECS.png", alt: "logo FEPECS" }}
+          name="Intern FEPECS"
+          duration={{ start: "2024", end: "CURRENT" }}
+          description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus
           sunt, reprehenderit illum suscipit iure corporis, unde aliquid
           nemo odio voluptatibus, eos tempore. Debitis fugiat magnam eum
           eius autem obcaecati. Nihil."
-            tools={[
-              "WordPress",
-              "Javascript",
-              "Typescript",
-              "React",
-              "Next.js",
-            ]}
-          />
+          tools={["WordPress", "Javascript", "Typescript", "React", "Next.js"]}
+        />
 
-          <a href="#">
-            <span className="font-semibold">View full résumé</span>
-          </a>
+        <a href="#">
+          <span className="font-semibold">View full résumé</span>
+        </a>
+      </div>
+      <div className="flex flex-col gap-12">
+        <div className="text-center font-bold text-4xl lg:text-xl mt-20">
+          My Projects:{" "}
         </div>
-        <div className="flex flex-col gap-24">
-          <div className="text-center font-bold text-4xl lg:text-xl mt-20">
-            My Projects:{" "}
-          </div>
-          <ProjectsCard
-            href="#"
-            imageData={{
-              src: "/Agendamento-de-salas.jpeg",
-              alt: 'Home of my website "Agendamento de salas FEPECS"',
-            }}
-            name="Agendamento de salas FEPECS"
-            description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus
+        <ProjectsCard
+          href="#"
+          imageData={{
+            src: "/Agendamento-de-salas.jpeg",
+            alt: 'Home of my website "Agendamento de salas FEPECS"',
+          }}
+          name="Agendamento de salas FEPECS"
+          description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus
           sunt, reprehenderit illum suscipit iure corporis, unde aliquid
           nemo odio voluptatibus, eos tempore. Debitis fugiat magnam eum
           eius autem obcaecati. Nihil."
-            tools={["Typescript", "Next.js", "Tailwind", "Supabase"]}
-          />
+          tools={["Typescript", "Next.js", "Tailwind", "Supabase"]}
+        />
 
-          <a href="#">
-            <span className="font-semibold">View all projects</span>
-          </a>
-        </div>
+        <a href="#">
+          <span className="font-semibold">View all projects</span>
+        </a>
       </div>
     </div>
   );
