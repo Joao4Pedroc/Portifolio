f470e153d79f4aa2877d13fd43a17f236f0597c1
Joao4Pedroc <joao4pedroc@gmail.com>
2024-09-26 14:11:33 -0300

    Feat: DASHES

--- app/_components/LeftSidebar.tsx
+++ app/_components/LeftSidebar.tsx
@@ -10,9 +10,26 @@ function LeftSidebar() {
           nana eu te amoo❤️
         </h3>
         <nav className="lg:flex flex-col hidden">
-          <span>--- Experiences</span>
-          <span>--- Projets</span>
-          <span>--- Skills</span>
+          <ul>
+            <li className="my-2">
+              <a href="#experiences" className="group inline-flex items-center">
+                <span className="w-12 group-hover:w-20 border-t-2 border-zinc-400 mr-2 group-hover:border-zinc-300 transition-all motion-reduce:transition-none"></span>
+                <span>Experiences</span>
+              </a>
+            </li>
+            <li className="my-2">
+              <a href="#projects" className="group inline-flex items-center">
+                <span className="w-12 group-hover:w-20 border-t-2 border-zinc-400 mr-2 group-hover:border-zinc-300 transition-all motion-reduce:transition-none"></span>
+                <span>Projects</span>
+              </a>
+            </li>
+            <li className="my-2">
+              <a href="#skills" className="group inline-flex items-center">
+                <span className="w-12 group-hover:w-20 border-t-2 border-zinc-400 mr-2 group-hover:border-zinc-300 transition-all motion-reduce:transition-none"></span>
+                <span>Skills</span>
+              </a>
+            </li>
+          </ul>
         </nav>
       </div>
 
--- app/_components/RightContent.tsx
+++ app/_components/RightContent.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 function RightContent({ children }: { children: ReactNode }) {
-  return <main className="pt-24 lg:w-1/2">{children}</main>;
+  return <main className="pt-0 lg:pt-24 lg:w-1/2">{children}</main>;
 }
 
 export default RightContent;
