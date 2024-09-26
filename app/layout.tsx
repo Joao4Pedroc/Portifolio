9b474c283a60d5ce6ca70c7d7448f55904836d4f
Joao4Pedroc <joao4pedroc@gmail.com>
2024-09-26 13:43:23 -0300

    Feat: icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: "Portífolio",
   description: "Portífolio do João Pedro",
+  icons: "icon-kuromi.svg",
 };
 
 export default function RootLayout({
